fbbca6a4dac5668fd1e694a6c1aed17f18e1b524
jessicabreedlove <jessicabreedlove@gmail.com>
2022-05-02 13:32:52 -0600

    my contacts show up locallt

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,35 +1,26 @@
 const routes = require("express").Router();
 
 routes.get("/", (req, res) => {
-  res.send("This is for my contacts");
+  //ugly code
+
+  const dotenv = require("dotenv");
+  dotenv.config();
+
+  const MongoClient = require("mongodb").MongoClient;
+  MongoClient.connect(process.env.MONGODB_URI, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("cse341-1");
+    dbo
+      .collection("contacts")
+      .find()
+      .toArray(function (err, result) {
+        if (err) throw err;
+        res.json(result);
+        db.close();
+      });
+  });
 });
 
 module.exports = routes;
 
-// not currently working
-
-// const { MongoClient } = require("mongodb");
-
-// async function main() {
-//   const client = new MongoClient(process.env.MONGODB_URI);
-
-//   try {
-//     await client.connect();
-//     await listDatabases(client);
-//   } catch (e) {
-//     console.error(e);
-//   } finally {
-//     await client.close();
-//   }
-// }
-
-// main().catch(console.error);
-
-// async function listDatabases(client) {
-//   const databasesList = await client.db().admin.listDatabases();
-
-//   console.log("Databases:");
-//   databasesList.databases.forEach((db) => {
-//     console.log(`- ${db.name}`);
-//   });
-// }
+//week 2
--- server.js
+++ server.js
@@ -13,25 +13,5 @@ const port = process.env.PORT || 3000;
 app.use("/", require("./routes"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-
-//week 2
-
-const dotenv = require("dotenv");
-dotenv.config();
-
-//ugly code
-const MongoClient = require("mongodb").MongoClient;
-MongoClient.connect(process.env.MONGODB_URI, function (err, db) {
-  if (err) throw err;
-  var dbo = db.db("cse341-1");
-  dbo
-    .collection("contacts")
-    .find()
-    .toArray(function (err, result) {
-      if (err) throw err;
-      console.log(result);
-      db.close();
-    });
+  console.log(`App listening on port ${port}`);
 });
