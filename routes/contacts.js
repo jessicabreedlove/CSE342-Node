ce4722a5ae30dde03b213e9fd6a8919300d9d3b9
jessicabreedlove <jessicabreedlove@gmail.com>
2022-05-02 17:35:18 -0600

    added our REST endpoints

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,15 +1,25 @@
 const routes = require("express").Router();
+const { ObjectId } = require("mongodb");
 const connect = require("../db/connect");
+const ObjectID = require("mongodb").ObjectId;
 
 routes.get("/", (req, res) => {
-  connect
-    .getCollection()
-    .find()
-    .toArray((err, result) => {
-      if (err) throw err;
-      res.json(result);
-      console.log("Contacts query successful");
-    });
+  const results = connect.getCollection().find();
+
+  results.toArray().then((documents) => {
+    res.status(200).json(documents);
+    console.log("Returned ALL Contacts");
+  });
+});
+
+routes.get("/:id", (req, res) => {
+  contactId = new ObjectId(req.params.id);
+  const results = connect.getCollection().find({ _id: contactId });
+
+  results.toArray().then((documents) => {
+    res.status(200).json(documents[0]);
+    console.log(`Returned Contact ${req.params.id}`);
+  });
 });
 
 module.exports = routes;
