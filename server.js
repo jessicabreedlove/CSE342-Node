029a714bfad8583225519f1063d76cd953ae32c2
jessicabreedlove <jessicabreedlove@gmail.com>
2022-05-07 14:52:56 -0600

    added body parser

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
-const connect = require("./db/connect");
+const connect = require('./db/connect');
 
 connect.initDatabase();
 
-app.use("/", require("./routes"));
+app.use(bodyParser.json()).use('/', require('./routes'));
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
