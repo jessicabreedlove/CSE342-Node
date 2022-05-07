7e4538a21a3241fb7396cbe847b2b3106bfe9f26
jessicabreedlove <jessicabreedlove@gmail.com>
2022-05-07 15:17:15 -0600

    jshint

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,18 +1,52 @@
-const routes = require("express").Router();
-const { ObjectId } = require("mongodb");
-const connect = require("../db/connect");
-const ObjectID = require("mongodb").ObjectId;
+const routes = require('express').Router();
+const { ObjectId } = require('mongodb');
+const connect = require('../db/connect');
+const ObjectID = require('mongodb').ObjectId;
 
-routes.get("/", (req, res) => {
+routes.get('/', (req, res) => {
   const results = connect.getCollection().find();
 
   results.toArray().then((documents) => {
     res.status(200).json(documents);
-    console.log("Returned ALL Contacts");
+    console.log('Returned ALL Contacts');
   });
 });
 
-routes.get("/:id", (req, res) => {
+routes.get('/:id', (req, res) => {
+  contactId = new ObjectId(req.params.id);
+  const results = connect.getCollection().find({ _id: contactId });
+
+  results.toArray().then((documents) => {
+    res.status(200).json(documents[0]);
+    console.log(`Returned Contact ${req.params.id}`);
+  });
+});
+
+// my attempt at post, put and delete
+
+// Post
+routes.post('/', (req, res) => {
+  const results = connect.getCollection().find();
+
+  results.toArray().then((documents) => {
+    res.status(200).json(documents);
+    console.log('Posted Contacts');
+  });
+});
+
+// Put
+routes.put('/:id', (req, res) => {
+  contactId = new ObjectId(req.params.id);
+  const results = connect.getCollection().find({ _id: contactId });
+
+  results.toArray().then((documents) => {
+    res.status(200).json(documents[0]);
+    console.log(`Returned Contact ${req.params.id}`);
+  });
+});
+
+// Delete
+routes.delete('/:id', (req, res) => {
   contactId = new ObjectId(req.params.id);
   const results = connect.getCollection().find({ _id: contactId });
 
