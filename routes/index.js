65d60faddbdbc3d3b5f5cde674e8f19a6eef3b49
jessicabreedlove <jessicabreedlove@gmail.com>
2022-05-14 20:41:25 -0600

    changed route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const routes = require('express').Router();
 
-routes.use('/', require('./swagger'));
+// routes.use('/', require('./swagger'));
 routes.use('/', require('./home'));
 routes.use('/contacts', require('./contacts'));
 
