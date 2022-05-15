3d88201ece737002c4d962aa3c7a98022edb24c6
jessicabreedlove <jessicabreedlove@gmail.com>
2022-05-14 20:43:11 -0600

    header

--- server.js
+++ server.js
@@ -17,6 +17,26 @@ const swaggerDocument = require('../swagger-output.json');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  res.setHeader('Access-Control-Expose-Headers', 'Content-Disposition');
+
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key, Content-Disposition'
+  );
+
+  // res.setHeader('Content-Type', 'application/json');
+
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS'
+  );
+
+  next();
+});
+
 // module.exports = router;
 
 app.listen(port, () => {
